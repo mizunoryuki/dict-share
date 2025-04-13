b33690c49c96c4056bd684040fb118a7aac6bc3c
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-04-13 17:16:31 +0900

    リダイレクト処理を追加

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -3,6 +3,9 @@ import DictContainer from "@/components/DictContainer";
 import Header from "@/components/Header";
 import styles from "./page.module.css";
 import { Button } from "@/components/Button";
+import { useAuth } from "@/context/AuthProvider";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const data = [
   {
@@ -20,15 +23,38 @@ const data = [
 ];
 
 export default function Mypage() {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.replace("/");
+    }
+  }, [user, loading, router]);
+
+  if (loading) {
+    return (
+      <div className={styles.containerRedirect}>
+        <p className={styles.message}>読み込み中</p>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return null;
+  }
+
   return (
-    <div className={styles.container}>
-      <Header />
-      <DictContainer data={data} />
-      <Button
-        color="secondary"
-        text="追加"
-        onClickAction={() => console.log("add")}
-      />
-    </div>
+    <>
+      <div className={styles.container}>
+        <Header />
+        <DictContainer data={data} />
+        <Button
+          color="secondary"
+          text="追加"
+          onClickAction={() => console.log("add")}
+        />
+      </div>
+    </>
   );
 }
