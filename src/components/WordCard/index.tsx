bb1cc1ce5104efa88c19e3993efea3737f8f4d8d
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-04-18 13:58:09 +0900

    編集アイコンの追加

--- src/components/WordCard/index.tsx
+++ src/components/WordCard/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./index.module.css";
 interface Props {
   name: string;
@@ -6,7 +7,16 @@ interface Props {
 export default function WordCard({ name, discription }: Props) {
   return (
     <div className={styles.wordContainer}>
-      <p className={styles.wordName}>{name}</p>
+      <div className={styles.nameBox}>
+        <p className={styles.wordName}>{name}</p>
+        <Image
+          src={"../editImage.svg"}
+          alt="edit"
+          width={30}
+          height={30}
+          onClick={() => console.log("edit card")}
+        />
+      </div>
       <div className={styles.wordDisc}>{discription}</div>
     </div>
   );
