eee3ada6b4b9061bb4829c26c5819bb723f06b32
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-04-24 14:21:17 +0900

    ログインしていたらマイページに飛ばす

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { useAuth } from "@/context/AuthProvider";
 import styles from "./page.module.css";
 import AuthCard from "@/components/AuthCard";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/mypage");
+    }
+  }, [router, user]);
   return (
     <div className={styles.page}>
       <AuthCard type="signIn" />
