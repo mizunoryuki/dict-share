ca5005e0e693b210b2d7e02a83e92ae745814bfa
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-04-29 23:32:23 +0900

    jsonの変換

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -2,7 +2,9 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK as string);
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_ADMIN_SDK!.replace(/\\n/g, "\n")
+);
 
 if (!getApps().length) {
   initializeApp({
