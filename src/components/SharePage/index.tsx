3f9079df59a8a643d0829990c06dacfcadf1ce0e
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-04-29 22:54:55 +0900

    suspensを使用するように変更

---
+++ src/components/SharePage/index.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import { useSearchParams } from "next/navigation";
+import Header from "@/components/Header";
+import ShareWordContainer from "@/components/ShareWordContainer";
+interface Data {
+  id: string;
+  dictName: string;
+  words: {
+	name: string;
+	discription: string;
+	wordId: string;
+	date: Date;
+  }[];
+}
+
+export default function SharePage() {
+  const searchParams = useSearchParams();
+  const dictId = searchParams.get("id");
+  const [dictData, setDictData] = useState<Data>({
+	id: "",
+	dictName: "",
+	words: [],
+  });
+  const [isError, setIsError] = useState<boolean>(false);
+  const [errorText, setErrorText] = useState<string>("");
+  useEffect(() => {
+	if (dictId) {
+	  const fetchData = async (dictId: string) => {
+		try {
+		  await fetch(`/api/share-dict?dictId=${dictId}`)
+			.then((res) => res.json())
+			.then((data) => {
+			  console.log(data);
+			  if (data.error) {
+				setIsError(true);
+				setErrorText(data.error);
+			  } else {
+				setDictData({ id: dictId, ...data });
+			  }
+			});
+		} catch (error) {
+		  console.log(error);
+		  setIsError(true);
+		}
+	  };
+	  fetchData(dictId);
+	} else {
+	  setIsError(true);
+	  setErrorText("idが選択されていません");
+	}
+  }, [dictId]);
+
+  return (
+	<div className={styles.container}>
+	  <Header isHide={false} />
+	  <ShareWordContainer
+		data={dictData}
+		errorText={errorText}
+		isError={isError}
+	  />
+	</div>
+  );
+}
