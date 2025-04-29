6d7c9a6a5a138e1684ef30c77dc76c1ce3718265
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-04-29 23:21:51 +0900

    変数名変更

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -2,9 +2,7 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceAccount = JSON.parse(
-  process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK as string
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK as string);
 
 if (!getApps().length) {
   initializeApp({
